fa234421586474e8362daf1a18c1ed8ade65483d
Caroline <liardcar@gmail.com>
2022-07-29 17:25:53 +0200

    chore: check file with Validator 3WC

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+"use strict";
+
+/* BURGER MENU */
+
 let sidenav = document.getElementById("mySidenav");
 let openBtn = document.getElementById("openBtn");
 let closeBtn = document.getElementById("closeBtn");
@@ -5,12 +9,45 @@ let closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
     sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     sidenav.classList.remove("active");
 }
+
+
+// 1 : PSEUDO-CODE ---------------------------------
+// Objectif : 
+// L'image doit changer toutes les 15 secondes.
+
+
+// 2 : CODE ----------------------------------------
+
+// Cibler les élèments
+let carousel = document.querySelector('.carousel');
+let timer;
+
+let images = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+];
+
+// Ajouter l'écouteur d'event
+carousel.addEventListener('click', changeUrl );
+
+let slideIndex = 1;
+
+function changeUrl() {
+    index++;
+    if ( index == images.length ) {
+        index = 0;
+    }
+    console.log(images[index]);
+    carousel.style.backgroundImage = "url('./images/" + images[index] + "')";
+};
+
+// Il y a surement une histoire de settimeout qqpart
